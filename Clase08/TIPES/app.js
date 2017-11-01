109f496a05f39b9a9c5c1c0f970fa35efbe524e5
alumno <alumno>
2017-10-31 22:09:58 -0300

    fin clase

---
+++ Clase08/TIPES/app.js
@@ -0,0 +1,8 @@
+///<reference path=''/>
+"use strict";
+// ES COMO UN ADDEVELISTENER LOAD
+$(function () { });
+{
+    $('#txt').val("Hola mundo");
+}
+;
---
+++ Clase08/TIPES/app.ts
@@ -0,0 +1,5 @@
+
+
+$(function()){
+    $('#txt').val("Hola mundo"); 
+});
