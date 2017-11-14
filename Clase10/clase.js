5fa08f8390e413af6e63d1f2822bf0e2b311c495
alumno <alumno>
2017-11-14 20:20:39 -0300

    MITAD CLASE

--- Clase10/clase.js
+++ Clase10/clase.js
@@ -1,29 +1,84 @@
-"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 //CLASES 
 //  PROPRIEDADES, CONSTRUCTORES, METODOS
-var Avenger = /** @class */ (function () {
-    function Avenger(nombre, nombreReal, peleasGanadas) {
-        this._nombre = nombre;
-        this.nombreReal = nombreReal;
-        this.peleasGanadas = peleasGanadas;
-    }
-    Avenger.prototype.mostrar = function () {
-        return this._nombre + ", " + this.nombreReal + ", " + this.peleasGanadas;
-    };
-    Object.defineProperty(Avenger.prototype, "nombre", {
-        get: function () {
-            return this._nombre;
-        },
-        set: function (nombre) {
+var EsClase10;
+(function (EsClase10) {
+    var Avenger = /** @class */ (function () {
+        function Avenger(nombreReal, peleasGanadas, nombre) {
+            //cuando pongo el signo de pregunta, tengo que sacar el strict del tsconfig.json
             this._nombre = nombre;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return Avenger;
-}());
-var a1 = new Avenger("Iron Man", "Tony", 10);
-// a1.nombreReal = "Tony";
-// a1.peleasGanadas = 10;
-console.log(a1.mostrar());
-console.log(a1);
+            this.nombreReal = nombreReal;
+            this.peleasGanadas = peleasGanadas;
+        }
+        Avenger.prototype.mostrar = function () {
+            return this._nombre + ", " + this.nombreReal + ", " + this.peleasGanadas;
+        };
+        Object.defineProperty(Avenger.prototype, "nombre", {
+            get: function () {
+                return this._nombre;
+            },
+            set: function (nombre) {
+                this._nombre = nombre;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return Avenger;
+    }());
+    EsClase10.Avenger = Avenger;
+    //OTRA CLASE, HEREDADA
+    var Xmen = /** @class */ (function (_super) {
+        __extends(Xmen, _super);
+        function Xmen(nr, pg, n, p) {
+            var _this = _super.call(this, nr, pg, n) || this;
+            _this._poder = p;
+            return _this;
+        }
+        Xmen.prototype.mostrar = function () {
+            return _super.prototype.mostrar.call(this) + " " + this._poder;
+        };
+        return Xmen;
+    }(Avenger));
+    //////////////////////
+    var Apocalipsis = /** @class */ (function () {
+        //COMO HACER PARA HACER 1 SOLO APOCALIPSIS QUE NO HAYA MAS
+        //EL CONSTRUCTOR ES PRIVADO 
+        function Apocalipsis(nombre) {
+            this.nombre = nombre;
+        }
+        Object.defineProperty(Apocalipsis, "Instance", {
+            //Instancio o no dependiendo si ya existe
+            get: function () {
+                if (!(this._instance)) {
+                    this._instance = new Apocalipsis("HEEELL");
+                }
+                return this._instance;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return Apocalipsis;
+    }());
+    var a1 = new Avenger("Iron Man", 10, "Tony");
+    var a2 = new Avenger("Bruce", 10);
+    var x1 = new Xmen("Logan", 2, "Wolverine", "garras mortales");
+    console.log(a1);
+    console.log(a1.mostrar());
+    console.log(a2.mostrar());
+    console.log(x1.mostrar());
+    //ARray de avenger
+    var array = new Array();
+    array.push(a1);
+    array.push(a2);
+    array.push(x1);
+    console.log(Apocalipsis.Instance);
+})(EsClase10 || (EsClase10 = {}));
--- Clase10/clase.ts
+++ Clase10/clase.ts
@@ -1,12 +1,14 @@
 //CLASES 
 //  PROPRIEDADES, CONSTRUCTORES, METODOS
-
-class Avenger{
-    private _nombre:string;
+namespace EsClase10{
+    
+export class Avenger{
+    private _nombre:string|undefined;
     nombreReal:string;
     peleasGanadas:number;
 
-    constructor (nombre:string,nombreReal:string, peleasGanadas:number){
+    constructor (nombreReal:string, peleasGanadas:number,nombre?:string){
+   //cuando pongo el signo de pregunta, tengo que sacar el strict del tsconfig.json
         this._nombre=nombre;
         this.nombreReal=nombreReal;
         this.peleasGanadas=peleasGanadas;
@@ -23,11 +25,58 @@ class Avenger{
     set nombre(nombre:string){
         this._nombre = nombre;
     }
+}
+
+//OTRA CLASE, HEREDADA
+class Xmen extends Avenger{
+    private _poder:string;
+    constructor(nr:string,pg:number,n:string,p:string){
+        super(nr,pg,n);
+        this._poder = p;
+        
+    }    
+
+    mostrar():string{
+        return super.mostrar() + " "+ this._poder;
+    }
 
 }
 
-let a1:Avenger = new Avenger("Iron Man","Tony",10);
-// a1.nombreReal = "Tony";
-// a1.peleasGanadas = 10;
-console.log(a1.mostrar());
+//////////////////////
+class Apocalipsis{
+    private static _instance:Apocalipsis;
+    //COMO HACER PARA HACER 1 SOLO APOCALIPSIS QUE NO HAYA MAS
+    //EL CONSTRUCTOR ES PRIVADO 
+    private constructor(public nombre:string){
+
+
+    }
+    //Instancio o no dependiendo si ya existe
+    static get Instance():Apocalipsis{
+        if(!(this._instance)){
+            this._instance = new Apocalipsis("HEEELL");
+        }
+        return this._instance;
+    }
+}
+
+
+let a1 : Avenger = new Avenger("Iron Man",10,"Tony");
+let a2:Avenger = new Avenger("Bruce",10);
+let x1:Xmen = new Xmen("Logan",2,"Wolverine","garras mortales")
+
 console.log(a1);
+console.log(a1.mostrar());
+console.log(a2.mostrar());
+console.log(x1.mostrar());
+
+//ARray de avenger
+let array = new Array<Avenger>();
+array.push(a1);
+array.push(a2);
+array.push(x1);
+
+console.log(Apocalipsis.Instance);
+
+
+}
